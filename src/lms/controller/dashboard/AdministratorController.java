6678403b37fa257038bc4c5b5bc04100e6d222ae
SanjeevForYou <yoursanjeev0@gmail.com>
2016-06-01 11:41:07 -0500

    changes in login controller and other respective dashboard

--- src/lms/controller/dashboard/AdministratorController.java
+++ src/lms/controller/dashboard/AdministratorController.java
@@ -1,7 +1,31 @@
 package lms.controller.dashboard;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
 /**
  * Created by Rajiv on 6/1/2016.
  */
-public class AdministratorController {
+public class AdministratorController implements Initializable {
+
+    @FXML
+    protected void addEditMemberClick() {
+    }
+
+    @FXML
+    protected void addBookCopyClick() {
+
+    }
+
+    @FXML
+    protected void addBookClick() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       // Utility.loadSearchMembersView(anchorPane);
+    }
 }
